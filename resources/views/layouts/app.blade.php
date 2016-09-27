c04e74db1fa6f4519c2d559540f060faf98ef9f8
lezwon <lezwon@gmail.com>
2016-09-27 20:00:52 +0530

    added https links for bower resources

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <!-- Styles -->
-    {{ Html::style('/bower_components/bootstrap/dist/css/bootstrap.min.css') }}
-    {{ Html::style('/bower_components/foundation/css/foundation.min.css') }}
+    {{ Html::style('/bower_components/bootstrap/dist/css/bootstrap.min.css' ,[],true ) }}
+    {{ Html::style('/bower_components/foundation-sites/dist/foundation.min.css',[],true) }}
     {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">--}}
 
      <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
