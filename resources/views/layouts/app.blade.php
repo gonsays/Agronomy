5943b182e178a20d338d05e699df0e4681126131
lezwon <lezwon@gmail.com>
2016-09-27 17:17:45 +0530

    updated bower

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <!-- Styles -->
     {{ Html::style('/bower_components/bootstrap/dist/css/bootstrap.min.css') }}
-    {{ Html::style('/bower_components/foundation/css/foundation.min.css') }}
+    {{ Html::style('/bower_components/foundation-sites/dist/foundation.min.css') }}
     {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">--}}
 
      <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
